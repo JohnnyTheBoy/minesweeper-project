f3c4efbc496c57ee865909aa20f61aa1a72e7638
Nikola Jandrić <nikola.jandric@outlook.com>
2018-02-17 18:33:14 +0100

    timer module created

---
+++ app/timer.ts
@@ -0,0 +1,53 @@
+let timer = {
+    time: {
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+    },
+    interval: 1,
+    timeIncrement: () => {
+        if (timer.time.seconds < 59) { timer.time.seconds++ }
+        else if (timer.time.seconds === 59 && timer.time.minutes < 59) { timer.time.seconds = 0, timer.time.minutes++ }
+        else if (timer.time.seconds === 59 && timer.time.minutes === 59) { timer.time.seconds = 0, timer.time.minutes = 0, timer.time.hours++ }
+    }
+};
+
+
+let getSeconds = (): number => {
+    return timer.time.seconds + (timer.time.minutes * 60) + (timer.time.hours * 360);
+};
+
+
+let getStringTime = () => {
+    let prevTime = [timer.time.hours, timer.time.minutes, timer.time.seconds];
+    let currTime = prevTime.map((timeElement) => { if (timeElement < 10) { return "0" + timeElement } else { return timeElement } });
+    return `${currTime[0]} : ${currTime[1]} : ${currTime[2]}`;
+};
+
+let startTimer = (step = 1): void => {
+    timer.interval = setInterval(timer.timeIncrement, step * 1);
+};
+
+let stopTimer = (): void => {
+    clearInterval(timer.interval);
+};
+
+let resetTimer = () => {
+    for (let element in timer.time) { timer.time[element] = 0 };
+}
+
+let stringInterval = 0; // za ispis string vremena
+const timerPlace = document.getElementById('timerPlace');
+
+let startTimerHandler = () => {
+    startTimer();
+    stringInterval = setInterval(() => { timerPlace.textContent = getStringTime() }, 100);
+};
+
+let stopTimerHandler = () => {
+    stopTimer();
+    clearInterval(stringInterval);
+};
+
+
+export { startTimerHandler, stopTimer, resetTimer, getStringTime, timerPlace, stopTimerHandler, getSeconds };
